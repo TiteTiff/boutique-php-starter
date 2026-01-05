fa1cf36340592deeb64f4a94c6591b04dc02dd3e
Tite Tiff <simonchautemps.tiffanie@gmail.com>
2026-01-05 14:34:45 +0100

    Jour 1 Exercice 2-Variables produit

---
+++ exercices/jour-01/produit.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Veste";
+var_dump($name);
+$price = "29.99";
+var_dump($price);
+$stock = 30;
+var_dump($stock);
+$onsale = false;
+var_dump($onsale);
+echo "Le produit : $name", " coûte : $price", "€.";
