c933e4fa337364c123d714a4d4241969e53d174c
Tite Tiff <simonchautemps.tiffanie@gmail.com>
2026-01-09 09:27:01 +0100

    Jour 05-Exercice1-Première fonction

---
+++ exercices/jour-05/premieres-fonctions.php
@@ -0,0 +1,18 @@
+<?php
+function greed()
+{
+    echo "Bienvenue sur la boutique!";
+}
+
+function greedClient()
+{
+    $name = "Emma";
+    echo "Bienvenue sur la boutique Emma!";
+}
+
+greed();
+greedClient();
+greed();
+greedClient();
+greed();
+greedClient();
